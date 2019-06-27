e5b510ff5230e1887ed6d5cb71299bbbc2af5f97
YFujii223 <ainsoph567@gmail.com>
2019-06-27 16:04:41 +0900

    Modify views and template

--- flask_app/views.py
+++ flask_app/views.py
@@ -1,6 +1,7 @@
+from flask import request, redirect, url_for, render_template, flash, session
 from flask_app import app
 
 
 @app.route('/')
 def show_entries():
-    return "Hello World!"
+    return render_template('entries/index.html')
