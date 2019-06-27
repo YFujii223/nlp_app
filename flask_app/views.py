77f348ce9654c0ec4824e635cd175eecb3bd8f7d
YFujii223 <ainsoph567@gmail.com>
2019-06-27 17:39:56 +0900

    Add input_txrt

--- flask_app/views.py
+++ flask_app/views.py
@@ -5,3 +5,7 @@ from flask_app import app
 @app.route('/')
 def show_entries():
     return render_template('entries/index.html')
+
+@app.route('/input_txt', methods = ['GET', 'POST'])
+def text_ipt():
+    return render_template('input_txt.html')
